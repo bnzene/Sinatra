8ff769f1768b39c2a6740590b7c74b7b962a0364
Eleanor Kavanagh-Brown <ekavanaghbrown@gmail.com>
2017-01-17 10:06:55 +0000

    Add name input form

--- app.rb
+++ app.rb
@@ -6,7 +6,14 @@ get '/' do
   "hello!"
 end
 
-get '/cat' do
+get '/random-cat' do
+  @name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
   erb(:index)
 end
 
